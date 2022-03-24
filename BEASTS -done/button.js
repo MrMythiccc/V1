29bc65647bbb4df32d8871c02ff8a99a1e8f2811
MrMythiccc <marius2003@live.dk>
2022-03-24 11:10:19 +0100

    Commit

---
+++ BEASTS -done/button.js
@@ -0,0 +1,8 @@
+function buttonhandle() {
+  var x = document.getElementById("mybtn");
+  if (x.style.display == "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
